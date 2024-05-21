9ddf0f73c8ac31b10c86fbd586171b81fe96bcb7
stormymayday <stormymayday7@gmail.com>
2024-05-20 20:49:36 -0700

    Implemented the didPressNumber and updateText methods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,11 +7,66 @@
 
 import UIKit
 
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+    case multiplication
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString: String = "0"
+    var currentMode: modes = .not_set
+    var savedNum: Int = 0
+    var lastButtonWasMode: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+    }
+    
+    @IBAction func didPressSubtract(_ sender: Any) {
+    }
+    
+    @IBAction func didPressMultiply(_ sender: Any) {
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        
+        let stringValue: String? = sender.titleLabel?.text
+        
+        labelString = labelString.appending(stringValue!)
+        
+        updateText()
+        
+    }
+    
+    func updateText() {
+        
+        guard let labelInt: Int = Int(labelString) else {
+            
+            return
+            
+        }
+        
+        label.text = "\(labelInt)"
+        
+    }
+    
+    func changeModes(newMode: modes) {
+        
     }
 
 
